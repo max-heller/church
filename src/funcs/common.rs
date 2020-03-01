833b1e06e1780817127172a0e125c46cd00a26bd
mxheller <heller.max1@gmail.com>
2020-03-01 12:32:51 -0500

    making rust segfault

--- src/funcs/common.rs
+++ src/funcs/common.rs
@@ -30,6 +30,30 @@ pub fn superpower() -> impl Computable<U2> {
     pr![cn![S; U1; Z], cn![power(); U3; id![U3, U1], id![U3, U3]]]
 }
 
+fn one_place_recursive_func<F, G>(f: F, g: G) -> impl Computable<U1>
+where
+    F: Computable<U1>,
+    G: Computable<U3>,
+{
+    cn![pr![f, g]; U1; id![U1, U1], id![U1, U1]]
+}
+
+pub fn predecessor() -> impl Computable<U1> {
+    one_place_recursive_func(Z, id![U3, U2])
+}
+
+pub fn difference() -> impl Computable<U2> {
+    pr!(id![U1, U1], cn![predecessor(); U3; id![U3, U3]])
+}
+
+pub fn antisignum() -> impl Computable<U1> {
+    cn![difference(); U1; cn![S; U1; Z], id![U1, U1]]
+}
+
+pub fn signum() -> impl Computable<U1> {
+    cn![difference(); U1; cn![S; U1; Z], antisignum()]
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -57,4 +81,31 @@ mod test {
         let pow = power();
         defined_eq!(pow.call(args![2, 3]), 8);
     }
+
+    #[quickcheck]
+    fn predecessor_is_predecessor(x: usize) -> bool {
+        Some(x.saturating_sub(1)) == predecessor().call(args![x])
+    }
+
+    #[quickcheck]
+    fn difference_is_difference(x: usize, y: usize) -> bool {
+        Some(x.saturating_sub(y)) == difference().call(args![x, y])
+    }
+
+    #[test]
+    fn antisignum_is_antisignum() {
+        let asg = antisignum();
+        defined_eq!(asg.call(args![0]), 1);
+        // defined_eq!(asg.call(args![1]), 1);
+    }
+
+    // #[quickcheck]
+    // fn antisignum_is_antisignum(x: usize) -> bool {
+    //     Some(1usize.saturating_sub(x)) == antisignum().call(args![x])
+    // }
+
+    // #[quickcheck]
+    // fn signum_is_signum(x: usize) -> bool {
+    //     Some(1usize.saturating_sub(1usize.saturating_sub(x))) == signum().call(args![x])
+    // }
 }
